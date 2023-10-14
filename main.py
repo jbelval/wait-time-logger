22e012d3db74902a018becda9e517569af4d8406
John Belval <jbelval0508@gmail.com>
2023-10-14 15:30:31 -0400

    Set up main file to run logger

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from src.logger import Logger
+from time import sleep
 
 if __name__ == '__main__':
-    l = Logger()
+    udp_socket = ('localhost', 50001)
+    l = Logger(udp_socket=udp_socket)
+    l.start()
+    sleep(1)
+
+    close_logger = False
+    while not close_logger:
+        instruction = ''
+        try:
+            instruction = input("Type exit to end logger.\n")
+        except Exception as e:
+            print(f'Exception occurred: {e}')
+            close_logger = True
+
+        if instruction.strip().upper() == 'EXIT':
+            close_logger = True
+        elif instruction != '':
+            print(f'Did not understand input: {instruction}')
+
+    print('Exiting')
+    l.stop()
